760b81fc3eea4e6ef9bd9a7a68609a29a58f5a7f
auriwan <auriwan.yasper@sci.ui.ac.id>
2022-08-17 11:42:13 +0700

    Splash Activity

--- app/src/main/java/com/elthobhy/storyapp/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/elthobhy/storyapp/ui/splash/SplashActivity.kt
@@ -1,12 +1,39 @@
 package com.elthobhy.storyapp.ui.splash
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.util.Pair
 import com.elthobhy.storyapp.R
+import com.elthobhy.storyapp.databinding.ActivitySplashBinding
+import com.elthobhy.storyapp.ui.auth.LoginActivity
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper())
+            .postDelayed({
+                val optionsCompat: ActivityOptionsCompat =
+                    ActivityOptionsCompat.makeSceneTransitionAnimation(
+                        this,
+                        Pair(binding.storyText, "storyText"),
+                        Pair(binding.appText,"appText")
+                    )
+
+
+                startActivity(Intent(this, LoginActivity::class.java), optionsCompat.toBundle())
+                finishAffinity()
+            },2000)
     }
 }
